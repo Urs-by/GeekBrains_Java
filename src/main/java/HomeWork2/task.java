5f782b4fa4d97a861bf43fef7dbd136681cc174e
Artur.by <urs.by@iclood.com>
2022-12-13 16:05:19 +0300

    add Logger

--- src/main/java/HomeWork2/task.java
+++ src/main/java/HomeWork2/task.java
@@ -3,32 +3,41 @@ package HomeWork2;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 
 public class task {
-    static Logger logger = Logger.getLogger("FileNotFound");
+    static Logger logger = Logger.getLogger(task.class.getName());
 
     public static void main(String[] args) throws FileNotFoundException {
+        logger.setLevel(Level.ALL);
+        ConsoleHandler ch = new ConsoleHandler();
+        ch.setLevel(Level.ALL);
+        logger.addHandler(ch);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        ch.setFormatter(sFormat);
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите 1 чтобы решить первую задачу или 2 для решения второй: ");
-        int number = scanner.nextInt();
-        if (number == 1) {
-            boolean result = task2_1();
-            if (result) {
-                System.out.printf("Введенная строка является палиндромом, результат: %s", result);
+        System.out.print("Введите 1, чтобы решить первую задачу или 2 - для решения второй: ");
+        try {
+            int number = scanner.nextInt();
+            if (number == 1) {
+                boolean result = task2_1();
+                if (result) {
+                    System.out.printf("Введенная строка является палиндромом, результат: %s", result);
+                } else {
+                    System.out.printf("Введенная строка не является палиндромом, результат: %s", result);
+                }
+            } else if (number == 2) {
+                int count = 100;
+                String word = "TEST";
+                task2_2(count, word);
             } else {
-                System.out.printf("Введенная строка не является палиндромом, результат: %s", result);
+                System.out.print("Вы ввели неверное число");
             }
-        } else if (number == 2) {
-            int count = 100;
-            String word = "TEST";
-            task2_2(count, word);
-        } else {
-            System.out.print("Вы ввели неверное число");
+        } catch (NumberFormatException e) {
+            logger.info("Не верный формат данных!" + e.getMessage());
+            logger.log(Level.WARNING, "ASDFGH");
         }
     }
 
@@ -58,20 +67,16 @@ public class task {
 
         String created_stroke = sb.toString();
         write_file(created_stroke);
-        System.out.print("Данные успешно записаны в файл");
+        System.out.printf("Данные с значением %s успешно записаны в файл", word);
     }
 
     private static void write_file(String data) throws FileNotFoundException {
-        logger.setLevel(Level.ALL);
-        Handler handler = new ConsoleHandler();
-        handler.setLevel(Level.ALL);
-        logger.addHandler(handler);
-        try (PrintWriter pw = new PrintWriter("src/main/resources/files/task22.txt")) {
+
+        try (PrintWriter pw = new PrintWriter("src/main/resources/files/task3.txt")) {
             pw.print(data);
         } catch (FileNotFoundException e) {
-            logger.severe("File not found" + e.getMessage());
+            logger.severe("FileNotFound" + e.getMessage());
         }
     }
-
 }
 
