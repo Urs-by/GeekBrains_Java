52e4041adfe3bcb16105d234bd92939ef459b1bc
Artur.by <urs.by@iclood.com>
2022-12-21 13:43:28 +0300

    task 1-4 from Homework4 add metod2

--- src/main/java/HomeWork4/Homework4.java
+++ src/main/java/HomeWork4/Homework4.java
@@ -1,3 +1,5 @@
+package HomeWork4;
+
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -5,59 +7,60 @@ import java.util.Random;
 
 public class Homework4 {
     public static void main(String[] args) {
+        System.out.println(metod1());
+        System.out.println(metod2());
+        metod3();
+    }
+
+    private static LinkedList<Integer> createList() {
         LinkedList<Integer> ll = new LinkedList<>();
         Random random = new Random();
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 5; i++) {
             ll.add(random.nextInt(20));
         }
         System.out.println("Исходный числовой лист ");
         System.out.println(ll);
-        System.out.println("Реверсивный числовой лист ");
-        System.out.println(metod1(ll));
-//        System.out.println("Второй метод ");
-//        System.out.println(metod2(ll));
-
-        metod3();
+        return ll;
     }
 
     private static void metod3() {
         LinkedList<String> strList = new LinkedList<>();
-        Collections.addAll(strList,"папа","мама","сын","дочь","кот");
+        Collections.addAll(strList, "папа", "мама", "сын", "дочь", "кот");
         System.out.println("Исходный текстовый лист");
         System.out.println(strList);
         System.out.println("Реверсивный текстовый список ");
         Iterator iterator = strList.descendingIterator();
         while (iterator.hasNext()) {
-//            System.out.printf("%d, ",iterator.next());
             System.out.print(iterator.next() + " ");
         }
-
     }
 
-//    private static LinkedList<Integer> metod2(LinkedList<Integer> ll) {
-//
-//        int temp =ll.size();
-//        for (int i = 0; i < ll.size(); i++) {
-//            temp = ll.get(i);
-//            ll.set(i,ll.getLast());
-//            ll.removeLast();
-//        }
-//        //ll.removeLast();
-//
-//        return ll;
-//    }
 
-    private static LinkedList<Integer> metod1(LinkedList<Integer> ll) {
-        //1. Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+    private static LinkedList<Integer> metod2() {
+        System.out.println("Второй метод ");
+        LinkedList<Integer> ll = createList();
+        for (int i = 0; i < ll.size(); i++) {
+            ll.add(i, ll.getLast());
+            ll.removeLast();
+        }
+        System.out.println("Реверсивный числовой лист ");
+        return ll;
+    }
 
+
+    private static LinkedList<Integer> metod1() {
+        System.out.println("Первый метод ");
+        LinkedList<Integer> ll = createList();
         int temp = 0;
         int lenList = ll.size();
-
         for (int i = 0; i < (lenList / 2); i++) {
             temp = ll.get(lenList - 1 - i);
             ll.set(lenList - 1 - i, ll.get(i));
             ll.set(i, temp);
         }
+        System.out.println("Реверсивный числовой лист ");
         return ll;
     }
+
+
 }
